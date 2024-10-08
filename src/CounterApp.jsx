fb0d21c51859cbb67d340cc8d79331093381fdc6
EdChambers <titofarinas@outlook.com>
2024-10-07 23:35:11 -0600

    hooks app counter

---
+++ src/CounterApp.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+
+const sumar() = ({
+    setCounter(counter1 + 1 ),
+
+})
+
+export const CounterApp = () => {
+  const [{ counter1, counter2, counter3 }, setCounter] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+
+
+  return (
+    <>
+      <h1>Counter: {counter1}</h1>
+      <h1>Counter: {counter2}</h1>
+      <h1>Counter: {counter3}</h1>
+      <hr />
+      <button
+        className="btn btn-primary"
+        onClick={() => setCounter(counter1 + 1)}
+      >
+        +1
+      </button>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HooksApp } from "./HooksApp";
+import "./index.css";
+import { CounterApp } from "./CounterApp";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HooksApp />
+    <CounterApp />
+  </StrictMode>
+);
